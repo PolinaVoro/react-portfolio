7d834ad903f08dfdf3dac45d0e67c269d41ffac3
PolinaVoro <117389927+PolinaVoro@users.noreply.github.com>
2023-05-06 14:08:32 +0200

    commit to git

---
+++ src/pages/project.js
@@ -0,0 +1,39 @@
+import BtmGitHub from "../components/btnGitHub/BtmGitHub";
+
+import {useParams} from "react-router-dom";
+import {projects} from "./../helpers/projectsList";
+
+
+const Project = () => {
+
+
+    const {id} = useParams();
+    const project = projects[id];
+
+    return (     
+    
+    
+    <main className="section">
+    <div className="container">
+        <div className="project-details">
+
+            <h1 className="title-1">{project.title}</h1>
+
+            <img src={project.imgBig} alt={project.title} className="project-details__cover" />
+
+            <div className="project-details__desc">
+                <p>Skills: {project.skills}</p>
+            </div>
+
+            {project.gitHubLink && (<BtmGitHub link="https://github.com" />)}
+
+            
+
+
+
+        </div>
+    </div>
+</main> );
+}
+ 
+export default Project;
